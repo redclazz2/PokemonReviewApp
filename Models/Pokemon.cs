0b56d301cca1dd11eca78a6eb143f4283a9be723
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2024-02-12 20:55:33 -0500

    Added Models or POCOs

---
+++ Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+	public class Pokemon
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
+
+//Models are sometimes also called "pocos" and in ASP.NET they contain the properties from a SQL table.
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+//Welcome to the .NET API Program.cs
+//In this file you can add modules to the API with the builder.
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +12,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//This the pipeline a HTTP request goes through:
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -23,3 +27,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+//Other stuff in the project are:
+//1.AppSettings.json - configurations for when the app is running
+//2.Properties/launchSettings.json - configurations for when the app is building
